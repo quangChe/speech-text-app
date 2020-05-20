e7244999eb81f4ac433b3bec8bc427068390ddbe
quangChe <quang.n.che@gmail.com>
2020-05-20 16:39:14 -0700

    Init: Speech-Text App Working with text to speech element of app first

---
+++ Assets/Scripts/TTS/TTSComponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TTSComponent : MonoBehaviour
+{
+    public Text inputText;
+    public Button submitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            TextToSpeech.instance.Initialize();
+        }
+
+        submitButton.onClick.AddListener(Speak);
+    }
+
+    private void Speak()
+    {
+        string line = inputText.text;
+        Debug.Log("***********" + line + "***********");
+
+        //if (Application.platform == RuntimePlatform.Android && line.Length > 0)
+        //{
+        //    TextToSpeech.instance.Speak(line);
+        //}
+    }
+}
