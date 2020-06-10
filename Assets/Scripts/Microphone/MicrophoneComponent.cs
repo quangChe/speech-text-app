01ac46d596c7605044dee3fe6d0d9e6a145a7449
quangChe <quang.n.che@gmail.com>
2020-06-09 18:51:12 -0700

    Added functionality to get value of loudness of mic input

--- Assets/Scripts/Microphone/MicrophoneComponent.cs
+++ Assets/Scripts/Microphone/MicrophoneComponent.cs
@@ -5,25 +5,73 @@ using UnityEngine.Audio;
 
 public class MicrophoneComponent : MonoBehaviour
 {
-    public AudioSource audio;
     private string microphone = null;
 
-    void Start()
+    public static float MicLoudness;
+    public static float MicLoudnessinDecibels;
+
+    private AudioClip audioClip;
+    private AudioClip recordedClip;
+    public bool isRecording;
+    int audioSampleWindow = 128;
+
+
+    void OnEnable()
     {
-        if (microphone == null) microphone = Microphone.devices[0];
-        Debug.Log(microphone + "*****");
-        audio = GetComponent<AudioSource>();
-        audio.clip = Microphone.Start(microphone, true, 10, 44100);
-        audio.loop = true;
-        while (!(Microphone.GetPosition(microphone) > 0))
-        {
-            audio.Play();
-        }
+        StartMic();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MicLoudness = MicrophoneLevelMax();
     }
+
+    public void StartMic()
+    {
+        if (microphone == null) microphone = Microphone.devices[0];
+        audioClip = Microphone.Start(microphone, true, 999, 44100);
+        isRecording = true;
+    }
+
+
+    public void StopMic()
+    {
+        Microphone.End(microphone);
+        isRecording = false;
+    }
+
+
+    // Grabs mic data and stores as audio clip
+    private float MicrophoneLevelMax()
+    {
+        float levelMax = 0;
+        float[] waveData = new float[audioSampleWindow];
+        int micPosition = Microphone.GetPosition(null) - (audioSampleWindow + 1); // null means the first microphone
+        if (micPosition < 0) return 0;
+        audioClip.GetData(waveData, micPosition);
+
+        // Getting a peak on the last 128 samples
+        for (int i = 0; i < audioSampleWindow; i++)
+        {
+            float wavePeak = waveData[i] * waveData[i];
+            if (levelMax < wavePeak)
+            {
+                levelMax = wavePeak;
+            }
+        }
+        return levelMax;
+    }
+
+    // Disable any background running recording;
+    void OnDisable()
+    {
+        StopMic();
+    }
+
+    void OnDestroy()
+    {
+        StopMic();
+    }
+
 }
