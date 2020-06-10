64513b0095b0e7e5a6d3ba5f28b8a834c141d5f9
quangChe <quang.n.che@gmail.com>
2020-06-09 18:10:21 -0700

    Added component with working microphone task for testing in the background. Right now it outputs the input microphone as an audio source.

---
+++ Assets/Scripts/Microphone/MicrophoneComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MicrophoneComponent : MonoBehaviour
+{
+    public AudioSource audio;
+    private string microphone = null;
+
+    void Start()
+    {
+        if (microphone == null) microphone = Microphone.devices[0];
+        Debug.Log(microphone + "*****");
+        audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(microphone, true, 10, 44100);
+        audio.loop = true;
+        while (!(Microphone.GetPosition(microphone) > 0))
+        {
+            audio.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
