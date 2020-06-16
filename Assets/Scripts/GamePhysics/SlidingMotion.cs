cb4de55a8fce84c5fe57f08e66e0d55871fb260f
quangChe <quang.n.che@gmail.com>
2020-06-16 11:19:52 -0700

    Added a hit box to game scene

--- Assets/Scripts/GamePhysics/SlidingMotion.cs
+++ Assets/Scripts/GamePhysics/SlidingMotion.cs
@@ -6,8 +6,7 @@ public class SlidingMotion : MonoBehaviour
 {
     public float slideSpeed = 5f;
     public Rigidbody2D rb;
-    public Vector2 movement = new Vector2(-1f, 0f);
-
+    private Vector2 movement = new Vector2(-1f, 0f);
 
     private void FixedUpdate()
     {
