b01a1c30af625e0e481a950d2478da2926769da6
quangChe <quang.n.che@gmail.com>
2020-06-16 09:38:15 -0700

    Added physics to the slidign word with sliding speed setting

---
+++ Assets/Scripts/GamePhysics/SlidingMotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingMotion : MonoBehaviour
+{
+    public float slideSpeed = 5.0F;
+    public Rigidbody2D rb;
+
+    private Vector2 movement = new Vector2(-1f, 0f);
+
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * slideSpeed * Time.deltaTime);
+    }
+}
