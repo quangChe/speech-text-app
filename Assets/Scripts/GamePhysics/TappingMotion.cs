7202a7089024a108ed2df0f0572d4b1787ff4c9f
quangChe <quang.n.che@gmail.com>
2020-06-16 18:26:41 -0700

    Waypoints for tap animation

---
+++ Assets/Scripts/GamePhysics/TappingMotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TappingMotion : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TapDown());
+    }
+
+    // Update is called once per frame
+    IEnumerator TapDown()
+    {
+
+    }
+}
