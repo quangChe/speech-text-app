7447de01da07a928fcf3177908118087d042358b
quangChe <quang.n.che@gmail.com>
2020-06-18 12:22:06 -0700

    Many fixes to correct Unity problems with script renaming

---
+++ Assets/Scripts/Controllers/SlidingWordController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingWordController : MonoBehaviour
+{
+    public float slideSpeed = 5f;
+    public Rigidbody2D rb;
+    private Vector2 movement = new Vector2(-1f, 0f);
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * slideSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Controllers/TapAnimationController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapAnimationController : MonoBehaviour
+{
+    public GameObject[] path;
+    public GameObject hand;
+    public AudioSource drumSound;
+    public GameObject textPrompt;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("t"))
+        {
+            StartCoroutine(AnimateTapping());
+        }
+    }
+
+    public IEnumerator AnimateTapping()
+    {
+        yield return StartCoroutine(HandDownMotion());
+        StartCoroutine(HandUpMotion());
+    }
+
+    private IEnumerator HandDownMotion()
+    {
+        int i = 0;
+
+        while (i < path.Length)
+        {
+            hand.transform.position = Vector3.MoveTowards(hand.transform.position,
+                path[i].transform.position, Time.deltaTime * 15);
+
+            if (hand.transform.position == path[i].transform.position) i++;
+
+            yield return null;
+        }
+
+
+        AddExtraEffects();
+    }
+
+    private IEnumerator HandUpMotion()
+    {
+        int i = path.Length - 1;
+
+        while (i >= 0)
+        {
+            hand.transform.position = Vector3.MoveTowards(hand.transform.position,
+                path[i].transform.position, Time.deltaTime * 15);
+
+            if (hand.transform.position == path[i].transform.position) i--;
+
+            yield return null;
+        }
+
+    }
+
+    private void AddExtraEffects()
+    {
+        drumSound.Play();
+    }
+}
