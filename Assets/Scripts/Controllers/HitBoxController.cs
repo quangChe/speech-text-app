57069ac1113c2623832e3b788ae78eb219adc909
quangChe <quang.n.che@gmail.com>
2020-07-07 12:52:33 -0700

    Added microphone to the game scene

--- Assets/Scripts/Controllers/HitBoxController.cs
+++ Assets/Scripts/Controllers/HitBoxController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class HitBoxController : MonoBehaviour
 {
     public TapAnimationController tapAnimation;
+    public MicrophoneComponent microphone;
+
     // Start is called before the first frame update
     void Start()
     {
