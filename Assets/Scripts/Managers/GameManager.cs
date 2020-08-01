b28c128446336a7e61be122292aa76c2d2570ace
quangChe <quang.n.che@gmail.com>
2020-07-31 18:26:37 -0700

    Json loading functionality added to load word list

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,14 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 public class GameManager : Singleton<GameManager>
 {
 
-    protected override void OnAwake() {}
+    protected override void OnAwake()
+    {
+        LoadWords();
+    }
+
+    public Dictionary<string, string[]> wordList;
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void LoadWords()
+    {
+        string wordsJsonString = Resources.Load<TextAsset>("Words/WordDictionary").text;
+        wordList = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(wordsJsonString);
+        Debug.Log(wordList["Single Syllable"][0]);
     }
 }
