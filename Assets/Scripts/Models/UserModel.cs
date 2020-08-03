3ce6e40d3792df5d137e361fd553c5341473f472
quangChe <quang.n.che@gmail.com>
2020-08-03 16:08:07 -0700

    Drafted models of data needed for game

---
+++ Assets/Scripts/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class UserModel
+{
+    public int id = 0;
+    public string name = null;
+    public string lastLogin = null;
+    public int totalReps = 0;
+    public int totalStars = 0;
+}
+
