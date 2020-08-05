ce8a1589a74dabad20cfae23cc0c188b6a2fe19b
quangChe <quang.n.che@gmail.com>
2020-08-05 14:16:10 -0700

    Added a LevelSelectManager and wired up to GameManager's database instance

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -9,34 +9,33 @@ public class GameManager : Singleton<GameManager>
         SingleSyllables, MutiSyllables, EasyPhrases, MediumPhrases, HardPhrases
     }
 
-    [System.NonSerialized] public Dictionary<string, string[]> completeWordList;
+    [System.NonSerialized] public Dictionary<string, string[]> fullWordList;
 
     [System.NonSerialized] public string[] selectedWordList = null;
 
     [System.NonSerialized] public DatabaseHelper db;
 
+    [System.NonSerialized] public UserModel player;
+
+
     protected override void OnAwake()
     {
-        InitializeDatabase();
+        InitializeData();
         LoadWords();
     }
 
-    private void InitializeDatabase()
+    private void InitializeData()
     {
         db = new DatabaseHelper();
+        player = db.Player;
     }
 
     private void LoadWords()
     {
         string wordsJsonString = Resources.Load
             <TextAsset>("Words/WordDictionary").text;
-        completeWordList = JsonConvert.DeserializeObject
+        fullWordList = JsonConvert.DeserializeObject
             <Dictionary<string, string[]>>(wordsJsonString);
-    }
-
-    public Dictionary<string, string[]> GetCompleteWordList()
-    {
-        return completeWordList;
     }
 
     public string[] GetSelectedWordList()
@@ -44,28 +43,39 @@ public class GameManager : Singleton<GameManager>
         return selectedWordList;
     }
 
+    public Dictionary<string, string[]> GetCompleteWordList()
+    {
+        return fullWordList;
+    }
+
     public void SetSelectedWordList(WordListCategories category)
     {
         switch (category)
         {
             case WordListCategories.SingleSyllables:
-                selectedWordList = completeWordList["Single Syllable"];
+                selectedWordList = fullWordList["Single Syllable"];
                 break;
             case WordListCategories.MutiSyllables:
-                selectedWordList = completeWordList["Multi Syllable"];
+                selectedWordList = fullWordList["Multi Syllable"];
                 break;
             case WordListCategories.EasyPhrases:
-                selectedWordList = completeWordList["Easy Phrases"];
+                selectedWordList = fullWordList["Easy Phrases"];
                 break;
             case WordListCategories.MediumPhrases:
-                selectedWordList = completeWordList["Medium Phrases"];
+                selectedWordList = fullWordList["Medium Phrases"];
                 break;
             case WordListCategories.HardPhrases:
-                selectedWordList = completeWordList["Hard Phrases"];
+                selectedWordList = fullWordList["Hard Phrases"];
                 break;
             default:
                 selectedWordList = null;
                 break;
         }
     }
+
+    public List<WordCategoryModel> GetWordCategories()
+    {
+        return db.WordCategories;
+    }
+
 }
---
+++ Assets/Scripts/Managers/SceneManagers/LevelSelectManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectManager : MonoBehaviour
+{
+    private List<WordCategoryModel> wordCategories = new List<WordCategoryModel>();
+    private GameManager gm;
+
+    // Use this for initialization
+    void Start()
+    {
+        gm = GameManager.instance;
+        GetLevelData();
+        BuildLevels();
+        CheckPlayerProgress();
+    }
+
+    private void GetLevelData()
+    {
+        gm = GameManager.instance;
+        wordCategories = gm.GetWordCategories();
+    }
+
+    private void BuildLevels()
+    {
+        foreach (WordCategoryModel category in wordCategories)
+        {
+
+        }
+    }
+
+    private void CheckPlayerProgress()
+    {
+
+    }
+}
