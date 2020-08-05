ab1a8bf5ef854c58ad3d70d094164b8e311b89c0
quangChe <quang.n.che@gmail.com>
2020-08-05 16:15:56 -0700

    Dynamically generate level items and resize item list

--- Assets/Scripts/Managers/SceneManagers/LevelSelectManager.cs
+++ Assets/Scripts/Managers/SceneManagers/LevelSelectManager.cs
@@ -4,34 +4,61 @@ using UnityEngine;
 
 public class LevelSelectManager : MonoBehaviour
 {
-    private List<WordCategoryModel> wordCategories = new List<WordCategoryModel>();
+    public GameObject levelItemPrefab;
+    public GameObject levelItemsContainer;
+
     private GameManager gm;
+    private List<WordCategoryModel> levelCategories = new List<WordCategoryModel>();
+    private Dictionary<string, int> levelRequirements = new Dictionary<string, int>();
+    private int starsRequiredPerLevel = 30;
 
     // Use this for initialization
     void Start()
     {
-        gm = GameManager.instance;
-        GetLevelData();
-        BuildLevels();
-        CheckPlayerProgress();
+        PrepLevelData();
+        SetLevelLockStatus();
+        GenerateLevelItems();
     }
 
-    private void GetLevelData()
+    private void PrepLevelData()
     {
         gm = GameManager.instance;
-        wordCategories = gm.GetWordCategories();
+        levelCategories = gm.GetWordCategories();
     }
 
-    private void BuildLevels()
+    private void SetLevelLockStatus()
     {
-        foreach (WordCategoryModel category in wordCategories)
+        int starCounter = 0;
+
+        foreach (WordCategoryModel category in levelCategories)
         {
-
+            levelRequirements.Add(category.categoryName, starCounter);
+            starCounter += starsRequiredPerLevel;
         }
     }
 
-    private void CheckPlayerProgress()
+    private void GenerateLevelItems()
     {
+        int playerStars = gm.player.totalStars;
+        float itemWidth = 0;
 
+        for (int i = 0; i < levelCategories.Count; i++) 
+        {
+            GameObject levelItem = Instantiate(levelItemPrefab, levelItemsContainer.transform);
+            RectTransform itemRect = levelItem.GetComponent<RectTransform>();
+            itemRect.localPosition = new Vector2(
+                (itemRect.rect.width * i) + itemRect.localPosition.x,
+                itemRect.localPosition.y);
+
+            itemWidth = itemRect.rect.width;
+        }
+
+        RectTransform containerRect = levelItemsContainer.GetComponent<RectTransform>();
+        RectTransform containerParentRect = containerRect.parent.GetComponent<RectTransform>();
+        containerRect.sizeDelta = new Vector2(itemWidth * levelCategories.Count,
+            containerRect.rect.height);
+        containerRect.localPosition = new Vector2(
+            containerRect.rect.width - containerParentRect.rect.width + containerRect.localPosition.x,
+            containerRect.localPosition.y);
     }
 }
